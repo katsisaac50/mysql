17a3d696104de55e772b56719e5a2f15e2d30073
katsisaac50 <33658908+katsisaac50@users.noreply.github.com>
2018-04-22 06:38:23 +0200

    Add files via upload

--- MyFirst mysql.sql
+++ MyFirst mysql.sql
@@ -1,8 +1,8 @@
-/*use lesson1mysql;
+use lesson1mysql;
  select  name,title from task inner join user on  task.user_id=user.id;
 select count(id) tasknumber from task;
 select count(id) withoutvaliddate from task where due_date is null ;
-/*select * from task;
+select * from task;
 select * from task where  status_id=3;
 select * from task where  status_id=1;
 select * from task where  status_id=3;
@@ -17,11 +17,19 @@ task
 ORDER BY created DESC 
 LIMIT 1;
 
-/*SELECT count(*) AS mycount, status_id
+SELECT count(*) AS mycount, status_id
 FROM task
 GROUP BY status_id;
 
-select  name,due_date from task inner join user on  task.user_id=user.id;*/
-select  status.name,title from task inner join status on  task.status_id=status.id;
+SELECT  name,due_date 
+FROM task inner join user on  task.user_id=user.id;
+SELECT  status.name,title 
+FROM task inner join status on  task.status_id=status.id;
 
-SELECT (status.name),count(task.id) as task_count FROM task inner join status on  task.status_id=status.id GROUP BY status.ID;
+SELECT (status.name),count(task.id) as task_count 
+FROM task inner join status on  task.status_id=status.id 
+GROUP BY status.ID;
+
+SELECT status.name, title AS myCount 
+FROM task inner join status on  task.status_id=status.id 
+ORDER BY myCount DESC;
